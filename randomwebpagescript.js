2d749a4db7afe3252196011c01a597c1edc8dbfe
pinaki-mohanty <piyushmishravirat18@gmail.com>
2025-05-03 22:52:46 +0530

    Add files via upload

---
+++ randomwebpagescript.js
@@ -0,0 +1,33 @@
+function game1() {
+    var answer = window.confirm("me AP ko kuch bolna chahata hu agar ap ko janna he click ok pls");
+
+    if (answer) {
+        window.open("https://youtu.be/ch4On_R9Hig?si=uQUOM0IxHpGmQJcJ", "_self");
+    } else {
+        alert("I HAVE A CRUSH ON U!");
+    }
+}
+
+function game2() {
+    var answer = window.confirm("Do you want to listen a song I dedicated for u?");
+
+    if (answer) {
+        window.open("https://youtu.be/TzT2hyNwCFs?si=9z_neqjnhRPap2_d", "_self");
+    } else {
+        alert("tere liye ek mast song dedicate kiya hu pls sun le.");
+    }
+}
+
+function game3() {
+    window.open("./MAAN KI BAAT.html", "_self");
+}
+
+function game4() {
+    var answer = window.confirm("can we be friends?");
+
+    if (answer) {
+        window.open("https://youtube.com/shorts/gPefxPE_4ss?si=L3xvjLxpRwmFjDJw", "_self");
+    } else {
+        window.open("https://youtu.be/284Ov7ysmfA?si=rCC6qQfhYmTA5l-x", "_self");
+    }
+}
